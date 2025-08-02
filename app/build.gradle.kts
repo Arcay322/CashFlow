e4e769a489b496b2f8674f03c3c21de4c8239707
arcay325 <arcay325@gmail.com>
2025-08-02 01:35:20 +0000

    ,,,,

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.cashflow"
-        minSdk = 24
+        minSdk = 26 // <--- ¡AQUÍ ESTÁ LA CORRECCIÓN!
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
